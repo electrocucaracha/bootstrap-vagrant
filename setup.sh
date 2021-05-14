2cf67863a31d028691aca1af90ab502b26136e07
Victor Morales <chipahuac@hotmail.com>
2021-05-14 10:12:53 -0700

    Bump QEMU to 6.0.0 version

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ msg="Summary \n"
 export PKG_VAGRANT_VERSION=${PKG_VAGRANT_VERSION:-2.2.16}
 export PKG_VIRTUALBOX_VERSION=6.1
 export PKG_QAT_DRIVER_VERSION=1.7.l.4.11.0-00001
-export PKG_QEMU_VERSION=5.1.0
+export PKG_QEMU_VERSION=6.0.0
 if [ "${DEBUG:-false}" == "true" ]; then
     set -o xtrace
     export PKG_DEBUG=true
